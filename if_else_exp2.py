36cd1cc0fb277d8529d11150db8bbeb79b0c1c89
Rvaishnav10 <97674235+Rvaishnav10@users.noreply.github.com>
2023-03-17 18:23:34 +0530

    Create if_else_exp2.py

---
+++ if_else_exp2.py
@@ -0,0 +1,9 @@
+a=int(input("enter a"))
+b=int(input("enter b"))
+c=int(input("enter c"))
+if a>b & a>c:
+  print("a is greater that is", a)
+elif b>a & b>c:
+    print("b is greater that is",b)
+else:
+  print("c is greater that is",c)
